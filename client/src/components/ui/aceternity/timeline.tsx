b71fe465059eed15ff035f91e5a39a39bbb0794b
Tarang Sachaniya <tarangsachaniya8@gmail.com>
2026-03-21 17:54:23 +0530

    Merge branch 'main' of github.com:tarangsachaniya/portofolio

--- client/src/components/ui/aceternity/timeline.tsx
+++ client/src/components/ui/aceternity/timeline.tsx
@@ -30,7 +30,7 @@ export function Timeline({ data }: { data: TimelineEntry[] }) {
         {data.map((item, index) => (
           <div key={index} className="flex justify-start pt-10 md:pt-16 md:gap-10">
             <div className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
-              <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-background border border-border flex items-center justify-center">
+              <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-background border border-border flex items-center justify-center hidden md:flex">
                 <div className="h-4 w-4 rounded-full bg-foreground/20 border border-border p-2" />
               </div>
               <h3 className="hidden md:block text-xl md:pl-20 md:text-4xl font-bold text-foreground">
